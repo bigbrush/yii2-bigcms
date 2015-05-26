58bb1037f0d2482ce8fed820e2ce4f58488ad207
philoweb <mj@bigbrush-agency.com>
2015-05-26 19:41:44 +0200

    minor changes to DeleteButton

--- cms/widgets/DeleteButton.php
+++ cms/widgets/DeleteButton.php
@@ -29,7 +29,7 @@ class DeleteButton extends PopoverButton
 {
     /**
      * @var array|yii\db\ActiveRecord a model used when rendering the form. Not relevant when [[action]] and
-     * [[popoverContent]] are set during widget configuration.
+     * [[content]] are set during widget configuration.
      */
     public $model;
     /**
@@ -37,9 +37,13 @@ class DeleteButton extends PopoverButton
      */
     public $action;
     /**
-     * @var string the button label. Overridden from parent impementation to check if this has a user provided value.
+     * @var string the button label. Overridden from parent impementation to set a different default value.
      */
-    public $label;
+    public $label = '<i class="fa fa-trash"></i>';
+    /**
+     * @var string whether the label should be HTML-encoded. Overridden from parent impementation to set a different default value.
+     */
+    public $encodeLabel = false;
     /**
      * @var string defines the button class.
      */
@@ -49,7 +53,7 @@ class DeleteButton extends PopoverButton
      */
     public $popoverText = '<i class="fa fa-check"></i>';
     /**
-     * @var string the options for the popover button.
+     * @var array the options for the popover button.
      */
     public $popoverOptions = [];
 
@@ -59,17 +63,13 @@ class DeleteButton extends PopoverButton
      */
     public function init()
     {
-        if ($this->model === null) {
+        if ($this->model === null && $this->action === null && $this->content === null) {
             throw new InvalidConfigException("The 'model' property must be set when using cms\widgets\DeleteButton.");
         }
         parent::init();
         Html::addCssClass($this->options, $this->buttonClass);
         $this->useHtml = true;
 
-        if ($this->label === null) {
-            $this->label = '<i class="fa fa-trash"></i>';
-            $this->encodeLabel = false;
-        }
         if ($this->title === null) {
             $this->title = '<strong>Sure?</strong>';
         }
@@ -85,10 +85,7 @@ class DeleteButton extends PopoverButton
     {
         $popover = Html::beginForm($this->action);
         if ($this->content === null) {
-            $popover .= Html::submitButton($this->popoverText, ArrayHelper::merge([
-                'class' => 'btn btn-success',
-            ], $this->popoverOptions));
-            $popover .= Html::hiddenInput('id', $this->model['id']);
+            $popover .= $this->getFormFields();
         } else {
             $popover .= $this->content;
         }
@@ -97,5 +94,20 @@ class DeleteButton extends PopoverButton
         $this->content = $popover;
 
         return parent::run();
+    }
+
+    /**
+     * Returns the default form fields used in the popover.
+     *
+     * @return string form fields as HTML.
+     */
+    public function getFormFields()
+    {
+        $fields = [];
+        $fields[] = Html::submitButton($this->popoverText, ArrayHelper::merge([
+            'class' => 'btn btn-success',
+        ], $this->popoverOptions));
+        $fields[] = Html::hiddenInput('id', $this->model['id']);
+        return implode("\n", $fields);
     }
 }
