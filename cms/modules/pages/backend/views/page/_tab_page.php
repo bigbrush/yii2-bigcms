cd47583be1fd8f12d6a790ab58c27be20fd72f00
philoweb <mj@bigbrush-agency.com>
2015-06-11 22:26:18 +0200

    Editor implentation upgraded with more UI elements

--- cms/modules/pages/backend/views/page/_tab_page.php
+++ cms/modules/pages/backend/views/page/_tab_page.php
@@ -25,6 +25,11 @@ use bigbrush\big\widgets\editor\Editor;
 </div>
 <div class="row">
     <div class="col-md-12">
-        <?= $form->field($model, 'content')->widget(Editor::className()) ?>
+        <?= $form->field($model, 'content')->widget(Editor::className(), [
+            'clientOptions' => [
+                'plugins' => 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste',
+                'toolbar' => 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+            ]
+        ]) ?>
     </div>
 </div>
