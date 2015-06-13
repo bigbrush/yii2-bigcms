985d59e1c85e2d55ac358cfa2e7b10fa99f9bf3a
philoweb <mj@bigbrush-agency.com>
2015-06-13 20:13:03 +0200

    Text block updated

--- cms/blocks/text/views/edit.php
+++ cms/blocks/text/views/edit.php
@@ -7,8 +7,21 @@
 
 use bigbrush\big\widgets\editor\Editor;
 ?>
-<div class="row">
-    <div class="col-md-9">
-        <?= $form->field($model, 'content')->widget(Editor::className()) ?>
-    </div>
+<div class="panel panel-default">
+	<div class="panel-heading">
+    	<strong><?= Yii::t('cms', 'Content') ?></strong>
+	</div>
+
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+                <?= $form->field($model, 'content')->widget(Editor::className(), [
+                    'clientOptions' => [
+                    'plugins' => 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste',
+                    'toolbar' => 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+                ]
+            ])->label(false) ?>
+            </div>
+        </div>
+	</div>
 </div>
