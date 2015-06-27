b45e71fdbe5382fd30b474abdd0b43b77ffc143c
philoweb <mj@bigbrush-agency.com>
2015-06-27 12:48:34 +0200

    updated loading of config files

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/common/config/bootstrap.php');
 
 $config = require(__DIR__ . '/common/config/web.php');
 
